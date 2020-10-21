94becdf579586b804a303ff62ba4685426346b32
Nick Main <nickmaster24@gmail.com>
2020-10-20 21:24:34 -0400

    Added comments to the type file

--- Graph.d.ts
+++ Graph.d.ts
@@ -1,3 +1,6 @@
+/**
+ * Interface for theme overrides.
+ */
 interface Theme {
     Name?: string;
     Background?: Color3;
@@ -6,11 +9,24 @@ interface Theme {
 }
 
 type Graph = {
+    /** The number of points rendered on the graph. */
     Resolution: number;
+
+    /** A dictionary of data sets to be graphed. */
     Data: { [key: string]: number[] };
+
+    /** Wheteher or not the bottom of the grpah should start at zero (as opposed to the minimum value) */
     BaselineZero: boolean;
 
+    /**
+     * Set a theme to be used for the graph.
+     * @param newTheme The theme to use
+     */
     Theme(newTheme: Theme): void;
+
+    /**
+     * Render the graph
+     */
     Render(): void;
 };
 
